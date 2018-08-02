52bf3ad77ffeae3f99156d40d655c13f4127bf62
allan nishigouri <allan.nishigouri@koin.com.br>
2018-08-02 16:49:32 -0300

    Criação do componente de mensagem

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -4,7 +4,7 @@ class NegociacoesView {
         this._elemento = elemento;
     }
 
-    _template() {
+    _template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -17,15 +17,40 @@ class NegociacoesView {
                 </thead>
                 
                 <tbody>
+                    ${
+                        model.negociacoes.map(n => `
+                            <tr>
+                                <td>${DateHelper.dataParaTexto(n.data)}</td>
+                                <td>${n.quantidade}</td>
+                                <td>${n.valor}</td>
+                                <td>${n.volume}</td>
+                            </tr>
+                        ` ).join('') //Join converte para string
+                    }
                 </tbody>
-                
                 <tfoot>
+                    <td colspan="3">
+                        
+                    </td>
+                    <td>
+                        ${
+                            //IISE - Immediatly instruction function expression
+                            /*
+                            (function() {
+                                let total = 0;
+                                model.negociacoes.forEach(n => total += n.volume);
+                                return total;
+                            })()*/
+
+                            model.negociacoes.reduce((total, n) => total + n.volume, 0.0) //0.0 representa o valor inicial do total
+                        }
+                    </td>
                 </tfoot>
             </table>
         `;
     }
 
-    update() {
-        this._elemento.innerHTML = this._template();
+    update(model) {
+        this._elemento.innerHTML = this._template(model);
     }
 }
